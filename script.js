09d3b8e09c38f69c623617f4eecf7d64b629ce70
SolisCommet <nhatquangvl2003@gmail.com>
2022-09-03 09:19:37 +0700

    Fix a typo, give project 1 text outline

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const links = [
-	'1_ExpandingCards',
-	'2_ProgressSteps',
-	'3_RotatingNavigation',
-	'4_HiddenSearchWidget',
-	'#',
-	'6_ScrollAnimation',
-	'7_SplitLandingPage',
-	'8_FormWaveAnimation',
-	'9_SoundBoard'
+const links = ['1_ExpandingCards', '2_ProgressSteps', '3_RotatingNavigation', '4_HiddenSearchWidget', '#', '6_ScrollAnimation',
+'7_SplitLandingPage', '8_FormWaveAnimation', '9_SoundBoard', '#', 
+
+'#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
+
+'#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
+
+'#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
+
+'#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
 ]
 
 const button = document.getElementById('go')
