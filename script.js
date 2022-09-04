b78bf268794a24cc8e2ab97e7421113ce306e2f2
SolisCommet <nhatquangvl2003@gmail.com>
2022-09-04 14:59:45 +0700

    Add link to Project 15

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const links = ['1_ExpandingCards', '2_ProgressSteps', '3_RotatingNavigation', '4_HiddenSearchWidget', 'https://josephpham324.github.io/HBD-TQT', '6_ScrollAnimation',
 '7_SplitLandingPage', '8_FormWaveAnimation', '9_SoundBoard', '10_DadJokes', 
 
-'11_EventKeyCodes', '#', '#', '#', '#', '#', '#', '#', '#', '#',
+'11_EventKeyCodes', '#', '#', '#', '15_IncrementingCounter', '#', '#', '#', '#', '#',
 
 '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
 
