d60fdea32e0a8211b9cc7caa3d3862f2ccbe793d
SolisCommet <nhatquangvl2003@gmail.com>
2022-09-05 09:48:16 +0700

    Finish project 12: FAQ Collapse

--- 12_FAQCollapse/script.js
+++ 12_FAQCollapse/script.js
@@ -4,6 +4,9 @@ const buttons = document.querySelectorAll('.faq .faq-toggle')
 
 buttons.forEach(button=>{
 	button.addEventListener('click',()=>{
+	if (!button.parentElement.classList.contains('active')){
+		window.location= `#${button.parentElement.id}`
+	}
 	button.parentElement.classList.toggle('active')
 })
 })
