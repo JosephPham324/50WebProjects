f805ca43a5ec585353968cbe712da9402a412a59
SolisCommet <nhatquangvl2003@gmail.com>
2022-09-06 10:33:13 +0700

    Edit the content of Project 12

--- 12_FAQCollapse/script.js
+++ 12_FAQCollapse/script.js
@@ -2,16 +2,6 @@ const faqs = document.querySelectorAll('.faq')
 
 const buttons = document.querySelectorAll('.faq .faq-toggle')
 
-buttons.forEach(button=>{
-	button.addEventListener('click',()=>{
-	if (!button.parentElement.classList.contains('active')){
-		window.location= `#${button.parentElement.id}`
-	}
-	button.parentElement.classList.toggle('active')
-})
-})
-
-
 faqs.forEach(faq=>{
 	faq.addEventListener('click',()=>{
 		if (!faq.classList.contains('active')){
