ae3aa084c4dcd082eaec1bb24d4c884912ec5399
SolisCommet <nhatquangvl2003@gmail.com>
2022-09-06 12:29:44 +0700

    Finish Project 13: Random Choice Picker

---
+++ 13_RandomChoicePicker/script.js
@@ -0,0 +1,55 @@
+const content = document.getElementById('textarea')
+
+const tags = document.getElementById('tags')
+
+content.addEventListener('input', ()=>{
+	const value = content.value
+
+	const choices = value.split(',')
+	tags.innerHTML = choices.map((choice)=>{if (choice!='')	return `<span class = "tag">${choice}</span>`})
+					   		.join('')
+})
+
+content.addEventListener('keydown', (key)=>{
+	if (key.keyCode == '13'){
+		setTimeout(()=>{
+			key.target.value = ''
+		},10)
+		randomSelect()
+	}
+
+})
+
+function randomSelect(){
+	const times = 30
+
+	const interval = setInterval(()=>{
+		const tag = selectTag()
+
+		highlightTag(tag)
+
+		setTimeout(()=>{
+			unhighlightTag(tag)
+		},100)
+	},100)
+
+	setTimeout(()=>{
+		clearInterval(interval)
+
+		highlightTag(selectTag())
+	},times * 100)
+}
+
+function selectTag(){
+	const tags = document.querySelectorAll('.tag')
+
+	return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag){
+	tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag){
+	tag.classList.remove('highlight')
+}
