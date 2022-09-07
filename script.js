c71149832d3c2545a9475e0a51371e253daaf398
SolisCommet <nhatquangvl2003@gmail.com>
2022-09-08 06:56:48 +0700

    Fix wrong link to Project 20

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const links = ['01_ExpandingCards', '02_ProgressSteps', '03_RotatingNavigation',
 '07_SplitLandingPage', '08_FormWaveAnimation', '09_SoundBoard', '10_DadJokes', 
 
 '11_EventKeyCodes', '12_FAQCollapse', '13_RandomChoicePicker', '14_AnimatedNavigation', '15_IncrementingCounter',
- '20_ClickingRippleEffect', '#', '#', '#', '#',
+ '#', '#', '#', '#', '20_ClickingRippleEffect',
 
 '21_DragNDrop', '#', '#', '#', '#', '#', '#', '#', '#', '#',
 
